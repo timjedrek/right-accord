fe17866b200dabbeb7916e41b47123d3336bb6ed
Raul Ospina <raminka13@hotmail.com>
2024-05-15 20:43:41 -0500

    Update: nav and footer links for locations

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,6 +4,9 @@ module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
+      backgroundImage: {
+        'location-sarasota': "url('~/assets/images/fgrid-image2_rnl0fg_c_scale,w_600.jpg')",
+      },
       colors: {
         primary: 'var(--aw-color-primary)',
         secondary: 'var(--aw-color-secondary)',
