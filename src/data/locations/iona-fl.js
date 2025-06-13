0565dd9d80f971276cce4e581b71d31d2500e194
lucascr <lcrodrigues1995@gmail.com>
2025-06-13 12:59:22 -0300

    Create iona-fl.js

---
+++ src/data/locations/iona-fl.js
@@ -0,0 +1,45 @@
+const data = {
+  "metaData": {
+    "title": "Home Health Care Iona, FL | RIGHT ACCORD Private Duty Home Health Care",
+    "ignoreTitleTemplate": true
+  },
+  "city": "Iona",
+  "stateShort": "FL",
+  "stateLong": "Florida",
+  "slug": "iona-fl",
+  "county": "Lee County",
+  "description": "RIGHT ACCORD Home Health Care proudly serves Iona with compassionate, in-home care services. Whether your loved one needs hourly support or 24-hour specialized care, we tailor every plan to their unique needs.",
+  "nearbyLocations": [
+    "<a href=\"/locations/cape-coral-fl\">Cape Coral</a>",
+    "<a href=\"/locations/fort-myers-beach-fl\">Fort Myers Beach</a>",
+    "<a href=\"/locations/mcgregor-fl\">Mcgregor</a>",
+    "<a href=\"/locations/fort-myers-fl\">Fort Myers</a>",
+    "<a href=\"/locations/estero-fl\">Estero</a>"
+  ],
+  "headerImages": [
+    "/src/assets/images/right-accord-health-florida-HOMEPAGE_FIRSTPICTURE.jpg",
+    "/src/assets/images/right-accord-health-florida-HOMEPAGE_ALZHAIMERS.jpg",
+    "/src/assets/images/right-accord-health-florida-HOMEPAGE_PARKINSONS.jpg",
+    "/src/assets/images/right-accord-health-florida-HOMEPAGE_AFTERSTROKE.jpg",
+    "/src/assets/images/right-accord-health-florida-HOMEPAGE_HOSPICE.jpg"
+  ],
+  "headLines": [
+    "In-Home Senior Care in Iona, FL",
+    "Dependable Hourly & 24-Hour Caregivers",
+    "Specialized Alzheimer\u2019s and Dementia Support",
+    "Trusted Care from a Local, Nurse-Owned Agency",
+    "Home Care That Feels Like Family"
+  ],
+  "keywords": [
+    "Iona home health care",
+    "in-home senior care Iona",
+    "dementia care Iona FL",
+    "private duty caregivers Iona",
+    "elderly care Iona Florida",
+    "companion care Iona",
+    "Florida senior care services",
+    "RIGHT ACCORD Iona"
+  ]
+};
+
+export default { data };
