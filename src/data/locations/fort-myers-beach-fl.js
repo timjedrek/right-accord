1f379949b7cca1845442e052cb86ba9bb1e7ca3b
lucascr <lcrodrigues1995@gmail.com>
2025-06-13 12:58:01 -0300

    Create fort-myers-beach-fl.js

---
+++ src/data/locations/fort-myers-beach-fl.js
@@ -0,0 +1,45 @@
+const data = {
+  "metaData": {
+    "title": "Home Health Care Fort Myers Beach, FL | RIGHT ACCORD Private Duty Home Health Care",
+    "ignoreTitleTemplate": true
+  },
+  "city": "Fort Myers Beach",
+  "stateShort": "FL",
+  "stateLong": "Florida",
+  "slug": "fort-myers-beach-fl",
+  "county": "Lee County",
+  "description": "RIGHT ACCORD Home Health Care proudly serves Fort Myers Beach with compassionate, in-home care services. Whether your loved one needs hourly support or 24-hour specialized care, we tailor every plan to their unique needs.",
+  "nearbyLocations": [
+    "<a href=\"/locations/iona-fl\">Iona</a>",
+    "<a href=\"/locations/cape-coral-fl\">Cape Coral</a>",
+    "<a href=\"/locations/mcgregor-fl\">Mcgregor</a>",
+    "<a href=\"/locations/estero-fl\">Estero</a>",
+    "<a href=\"/locations/bonita-springs-fl\">Bonita Springs</a>"
+  ],
+  "headerImages": [
+    "/src/assets/images/right-accord-health-florida-HOMEPAGE_FIRSTPICTURE.jpg",
+    "/src/assets/images/right-accord-health-florida-HOMEPAGE_ALZHAIMERS.jpg",
+    "/src/assets/images/right-accord-health-florida-HOMEPAGE_PARKINSONS.jpg",
+    "/src/assets/images/right-accord-health-florida-HOMEPAGE_AFTERSTROKE.jpg",
+    "/src/assets/images/right-accord-health-florida-HOMEPAGE_HOSPICE.jpg"
+  ],
+  "headLines": [
+    "In-Home Senior Care in Fort Myers Beach, FL",
+    "Dependable Hourly & 24-Hour Caregivers",
+    "Specialized Alzheimer\u2019s and Dementia Support",
+    "Trusted Care from a Local, Nurse-Owned Agency",
+    "Home Care That Feels Like Family"
+  ],
+  "keywords": [
+    "Fort Myers Beach home health care",
+    "in-home senior care Fort Myers Beach",
+    "dementia care Fort Myers Beach FL",
+    "private duty caregivers Fort Myers Beach",
+    "elderly care Fort Myers Beach Florida",
+    "companion care Fort Myers Beach",
+    "Florida senior care services",
+    "RIGHT ACCORD Fort Myers Beach"
+  ]
+};
+
+export default { data };
