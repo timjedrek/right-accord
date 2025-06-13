2ee3edc2293012adfe0bac1fb7cc7a0c06cb556e
lucascr <lcrodrigues1995@gmail.com>
2025-06-13 13:00:30 -0300

    Create nokomis-fl.js

---
+++ src/data/locations/nokomis-fl.js
@@ -0,0 +1,45 @@
+const data = {
+  "metaData": {
+    "title": "Home Health Care Nokomis, FL | RIGHT ACCORD Private Duty Home Health Care",
+    "ignoreTitleTemplate": true
+  },
+  "city": "Nokomis",
+  "stateShort": "FL",
+  "stateLong": "Florida",
+  "slug": "nokomis-fl",
+  "county": "Sarasota County",
+  "description": "RIGHT ACCORD Home Health Care proudly serves Nokomis with compassionate, in-home care services. Whether your loved one needs hourly support or 24-hour specialized care, we tailor every plan to their unique needs.",
+  "nearbyLocations": [
+    "<a href=\"/locations/venice-fl\">Venice</a>",
+    "<a href=\"/locations/osprey-fl\">Osprey</a>",
+    "<a href=\"/locations/vamo-fl\">Vamo</a>",
+    "<a href=\"/locations/gulf-gate-estates-fl\">Gulf Gate Estates</a>",
+    "<a href=\"/locations/bee-ridge-fl\">Bee Ridge</a>"
+  ],
+  "headerImages": [
+    "/src/assets/images/right-accord-health-florida-HOMEPAGE_FIRSTPICTURE.jpg",
+    "/src/assets/images/right-accord-health-florida-HOMEPAGE_ALZHAIMERS.jpg",
+    "/src/assets/images/right-accord-health-florida-HOMEPAGE_PARKINSONS.jpg",
+    "/src/assets/images/right-accord-health-florida-HOMEPAGE_AFTERSTROKE.jpg",
+    "/src/assets/images/right-accord-health-florida-HOMEPAGE_HOSPICE.jpg"
+  ],
+  "headLines": [
+    "In-Home Senior Care in Nokomis, FL",
+    "Dependable Hourly & 24-Hour Caregivers",
+    "Specialized Alzheimer\u2019s and Dementia Support",
+    "Trusted Care from a Local, Nurse-Owned Agency",
+    "Home Care That Feels Like Family"
+  ],
+  "keywords": [
+    "Nokomis home health care",
+    "in-home senior care Nokomis",
+    "dementia care Nokomis FL",
+    "private duty caregivers Nokomis",
+    "elderly care Nokomis Florida",
+    "companion care Nokomis",
+    "Florida senior care services",
+    "RIGHT ACCORD Nokomis"
+  ]
+};
+
+export default { data };
