b227bcc2d0ede59aedf5131eb2322f4732a0cbc4
lucascr <lcrodrigues1995@gmail.com>
2025-06-13 12:56:56 -0300

    Create bonita-springs-fl.js

---
+++ src/data/locations/bonita-springs-fl.js
@@ -0,0 +1,45 @@
+const data = {
+  "metaData": {
+    "title": "Home Health Care Bonita Springs, FL | RIGHT ACCORD Private Duty Home Health Care",
+    "ignoreTitleTemplate": true
+  },
+  "city": "Bonita Springs",
+  "stateShort": "FL",
+  "stateLong": "Florida",
+  "slug": "bonita-springs-fl",
+  "county": "Lee County",
+  "description": "RIGHT ACCORD Home Health Care proudly serves Bonita Springs with compassionate, in-home care services. Whether your loved one needs hourly support or 24-hour specialized care, we tailor every plan to their unique needs.",
+  "nearbyLocations": [
+    "<a href=\"/locations/estero-fl\">Estero</a>",
+    "<a href=\"/locations/fort-myers-beach-fl\">Fort Myers Beach</a>",
+    "<a href=\"/locations/iona-fl\">Iona</a>",
+    "<a href=\"/locations/mcgregor-fl\">Mcgregor</a>",
+    "<a href=\"/locations/cape-coral-fl\">Cape Coral</a>"
+  ],
+  "headerImages": [
+    "/src/assets/images/right-accord-health-florida-HOMEPAGE_FIRSTPICTURE.jpg",
+    "/src/assets/images/right-accord-health-florida-HOMEPAGE_ALZHAIMERS.jpg",
+    "/src/assets/images/right-accord-health-florida-HOMEPAGE_PARKINSONS.jpg",
+    "/src/assets/images/right-accord-health-florida-HOMEPAGE_AFTERSTROKE.jpg",
+    "/src/assets/images/right-accord-health-florida-HOMEPAGE_HOSPICE.jpg"
+  ],
+  "headLines": [
+    "In-Home Senior Care in Bonita Springs, FL",
+    "Dependable Hourly & 24-Hour Caregivers",
+    "Specialized Alzheimer\u2019s and Dementia Support",
+    "Trusted Care from a Local, Nurse-Owned Agency",
+    "Home Care That Feels Like Family"
+  ],
+  "keywords": [
+    "Bonita Springs home health care",
+    "in-home senior care Bonita Springs",
+    "dementia care Bonita Springs FL",
+    "private duty caregivers Bonita Springs",
+    "elderly care Bonita Springs Florida",
+    "companion care Bonita Springs",
+    "Florida senior care services",
+    "RIGHT ACCORD Bonita Springs"
+  ]
+};
+
+export default { data };
