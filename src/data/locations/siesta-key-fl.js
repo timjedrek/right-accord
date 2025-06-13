50e6ef7c0f4f20882a3dc5d403f0ec2ae057d2e0
lucascr <lcrodrigues1995@gmail.com>
2025-06-13 13:01:39 -0300

    Create siesta-key-fl.js

---
+++ src/data/locations/siesta-key-fl.js
@@ -0,0 +1,46 @@
+const data = {
+  metaData: {
+    title: 'Home Health Care Siesta Key, FL | RIGHT ACCORD Private Duty Home Health Care',
+    ignoreTitleTemplate: true,
+  },
+  city: 'Siesta Key',
+  stateShort: 'FL',
+  stateLong: 'Florida',
+  slug: 'siesta-key-fl',
+  county: 'Sarasota County',
+  description:
+    "RIGHT ACCORD proudly serves Siesta Key, FL with home care services that combine clinical expertise and heartfelt compassion. Let our local team support your loved one's health and independence.",
+  nearbyLocations: [
+    `<a href="/locations/sarasota-fl">Sarasota</a>`,
+    `<a href="/locations/osprey-fl">Osprey</a>`,
+    `<a href="/locations/longboat-key-fl">Longboat Key</a>`,
+    `<a href="/locations/lakewood-ranch-fl">Lakewood Ranch</a>`,
+    `<a href="/locations/venice-fl">Venice</a>`,
+  ],
+  headerImages: [
+    '/src/assets/images/right-accord-health-florida-HOMEPAGE_FIRSTPICTURE.jpg',
+    '/src/assets/images/right-accord-health-florida-HOMEPAGE_ALZHAIMERS.jpg',
+    '/src/assets/images/right-accord-health-florida-HOMEPAGE_PARKINSONS.jpg',
+    '/src/assets/images/right-accord-health-florida-HOMEPAGE_AFTERSTROKE.jpg',
+    '/src/assets/images/right-accord-health-florida-HOMEPAGE_HOSPICE.jpg',
+  ],
+  headLines: [
+    'In-Home Senior Care in Siesta Key, FL',
+    'Dependable Hourly & 24-Hour Caregivers',
+    'Specialized Alzheimer’s and Dementia Support',
+    'Trusted Care from a Local, Nurse-Owned Agency',
+    'Home Care That Feels Like Family',
+  ],
+  keywords: [
+    'siesta key home health care',
+    'siesta key in-home senior care',
+    'siesta key dementia care',
+    'siesta key private duty caregivers',
+    'siesta key elderly care',
+    'siesta key companion care',
+    'siesta key Florida senior care services',
+    'siesta key RIGHT ACCORD',
+  ],
+};
+
+export default { data };
