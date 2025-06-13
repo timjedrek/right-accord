897cd8827d0e07934b7eeb3c9b0d8af91436c461
lucascr <lcrodrigues1995@gmail.com>
2025-06-13 13:02:08 -0300

    Create vamo-fl.js

---
+++ src/data/locations/vamo-fl.js
@@ -0,0 +1,45 @@
+const data = {
+  "metaData": {
+    "title": "Home Health Care Vamo, FL | RIGHT ACCORD Private Duty Home Health Care",
+    "ignoreTitleTemplate": true
+  },
+  "city": "Vamo",
+  "stateShort": "FL",
+  "stateLong": "Florida",
+  "slug": "vamo-fl",
+  "county": "Sarasota County",
+  "description": "RIGHT ACCORD Home Health Care proudly serves Vamo with compassionate, in-home care services. Whether your loved one needs hourly support or 24-hour specialized care, we tailor every plan to their unique needs.",
+  "nearbyLocations": [
+    "<a href=\"/locations/gulf-gate-estates-fl\">Gulf Gate Estates</a>",
+    "<a href=\"/locations/osprey-fl\">Osprey</a>",
+    "<a href=\"/locations/bee-ridge-fl\">Bee Ridge</a>",
+    "<a href=\"/locations/southgate-fl\">Southgate</a>",
+    "<a href=\"/locations/fruitville-fl\">Fruitville</a>"
+  ],
+  "headerImages": [
+    "/src/assets/images/right-accord-health-florida-HOMEPAGE_FIRSTPICTURE.jpg",
+    "/src/assets/images/right-accord-health-florida-HOMEPAGE_ALZHAIMERS.jpg",
+    "/src/assets/images/right-accord-health-florida-HOMEPAGE_PARKINSONS.jpg",
+    "/src/assets/images/right-accord-health-florida-HOMEPAGE_AFTERSTROKE.jpg",
+    "/src/assets/images/right-accord-health-florida-HOMEPAGE_HOSPICE.jpg"
+  ],
+  "headLines": [
+    "In-Home Senior Care in Vamo, FL",
+    "Dependable Hourly & 24-Hour Caregivers",
+    "Specialized Alzheimer\u2019s and Dementia Support",
+    "Trusted Care from a Local, Nurse-Owned Agency",
+    "Home Care That Feels Like Family"
+  ],
+  "keywords": [
+    "Vamo home health care",
+    "in-home senior care Vamo",
+    "dementia care Vamo FL",
+    "private duty caregivers Vamo",
+    "elderly care Vamo Florida",
+    "companion care Vamo",
+    "Florida senior care services",
+    "RIGHT ACCORD Vamo"
+  ]
+};
+
+export default { data };
