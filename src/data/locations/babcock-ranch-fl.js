225c778cdb3c19774857f50bb7d88cf10370907d
lucascr <lcrodrigues1995@gmail.com>
2025-06-13 12:56:47 -0300

    Create babcock-ranch-fl.js

---
+++ src/data/locations/babcock-ranch-fl.js
@@ -0,0 +1,45 @@
+const data = {
+  "metaData": {
+    "title": "Home Health Care Babcock Ranch, FL | RIGHT ACCORD Private Duty Home Health Care",
+    "ignoreTitleTemplate": true
+  },
+  "city": "Babcock Ranch",
+  "stateShort": "FL",
+  "stateLong": "Florida",
+  "slug": "babcock-ranch-fl",
+  "county": "Charlotte/Lee County",
+  "description": "RIGHT ACCORD Home Health Care proudly serves Babcock Ranch with compassionate, in-home care services. Whether your loved one needs hourly support or 24-hour specialized care, we tailor every plan to their unique needs.",
+  "nearbyLocations": [
+    "<a href=\"/locations/fort-myers-fl\">Fort Myers</a>",
+    "<a href=\"/locations/lehigh-acres-fl\">Lehigh Acres</a>",
+    "<a href=\"/locations/mcgregor-fl\">Mcgregor</a>",
+    "<a href=\"/locations/solana-fl\">Solana</a>",
+    "<a href=\"/locations/cape-coral-fl\">Cape Coral</a>"
+  ],
+  "headerImages": [
+    "/src/assets/images/right-accord-health-florida-HOMEPAGE_FIRSTPICTURE.jpg",
+    "/src/assets/images/right-accord-health-florida-HOMEPAGE_ALZHAIMERS.jpg",
+    "/src/assets/images/right-accord-health-florida-HOMEPAGE_PARKINSONS.jpg",
+    "/src/assets/images/right-accord-health-florida-HOMEPAGE_AFTERSTROKE.jpg",
+    "/src/assets/images/right-accord-health-florida-HOMEPAGE_HOSPICE.jpg"
+  ],
+  "headLines": [
+    "In-Home Senior Care in Babcock Ranch, FL",
+    "Dependable Hourly & 24-Hour Caregivers",
+    "Specialized Alzheimer\u2019s and Dementia Support",
+    "Trusted Care from a Local, Nurse-Owned Agency",
+    "Home Care That Feels Like Family"
+  ],
+  "keywords": [
+    "Babcock Ranch home health care",
+    "in-home senior care Babcock Ranch",
+    "dementia care Babcock Ranch FL",
+    "private duty caregivers Babcock Ranch",
+    "elderly care Babcock Ranch Florida",
+    "companion care Babcock Ranch",
+    "Florida senior care services",
+    "RIGHT ACCORD Babcock Ranch"
+  ]
+};
+
+export default { data };
