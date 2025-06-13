2d78e8d082e8795fa1255fa4844f2a3ec63e2efe
lucascr <lcrodrigues1995@gmail.com>
2025-06-13 12:57:51 -0300

    Create estero-fl.js

---
+++ src/data/locations/estero-fl.js
@@ -0,0 +1,45 @@
+const data = {
+  "metaData": {
+    "title": "Home Health Care Estero, FL | RIGHT ACCORD Private Duty Home Health Care",
+    "ignoreTitleTemplate": true
+  },
+  "city": "Estero",
+  "stateShort": "FL",
+  "stateLong": "Florida",
+  "slug": "estero-fl",
+  "county": "Lee County",
+  "description": "RIGHT ACCORD Home Health Care proudly serves Estero with compassionate, in-home care services. Whether your loved one needs hourly support or 24-hour specialized care, we tailor every plan to their unique needs.",
+  "nearbyLocations": [
+    "<a href=\"/locations/bonita-springs-fl\">Bonita Springs</a>",
+    "<a href=\"/locations/fort-myers-beach-fl\">Fort Myers Beach</a>",
+    "<a href=\"/locations/mcgregor-fl\">Mcgregor</a>",
+    "<a href=\"/locations/iona-fl\">Iona</a>",
+    "<a href=\"/locations/cape-coral-fl\">Cape Coral</a>"
+  ],
+  "headerImages": [
+    "/src/assets/images/right-accord-health-florida-HOMEPAGE_FIRSTPICTURE.jpg",
+    "/src/assets/images/right-accord-health-florida-HOMEPAGE_ALZHAIMERS.jpg",
+    "/src/assets/images/right-accord-health-florida-HOMEPAGE_PARKINSONS.jpg",
+    "/src/assets/images/right-accord-health-florida-HOMEPAGE_AFTERSTROKE.jpg",
+    "/src/assets/images/right-accord-health-florida-HOMEPAGE_HOSPICE.jpg"
+  ],
+  "headLines": [
+    "In-Home Senior Care in Estero, FL",
+    "Dependable Hourly & 24-Hour Caregivers",
+    "Specialized Alzheimer\u2019s and Dementia Support",
+    "Trusted Care from a Local, Nurse-Owned Agency",
+    "Home Care That Feels Like Family"
+  ],
+  "keywords": [
+    "Estero home health care",
+    "in-home senior care Estero",
+    "dementia care Estero FL",
+    "private duty caregivers Estero",
+    "elderly care Estero Florida",
+    "companion care Estero",
+    "Florida senior care services",
+    "RIGHT ACCORD Estero"
+  ]
+};
+
+export default { data };
