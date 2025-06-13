134d155b1ca289a67ebab8cd1fc9318372a843c6
lucascr <lcrodrigues1995@gmail.com>
2025-06-13 13:00:22 -0300

    Create mcgregor-fl.js

---
+++ src/data/locations/mcgregor-fl.js
@@ -0,0 +1,45 @@
+const data = {
+  "metaData": {
+    "title": "Home Health Care Mcgregor, FL | RIGHT ACCORD Private Duty Home Health Care",
+    "ignoreTitleTemplate": true
+  },
+  "city": "Mcgregor",
+  "stateShort": "FL",
+  "stateLong": "Florida",
+  "slug": "mcgregor-fl",
+  "county": "Lee County",
+  "description": "RIGHT ACCORD Home Health Care proudly serves Mcgregor with compassionate, in-home care services. Whether your loved one needs hourly support or 24-hour specialized care, we tailor every plan to their unique needs.",
+  "nearbyLocations": [
+    "<a href=\"/locations/cape-coral-fl\">Cape Coral</a>",
+    "<a href=\"/locations/iona-fl\">Iona</a>",
+    "<a href=\"/locations/fort-myers-fl\">Fort Myers</a>",
+    "<a href=\"/locations/fort-myers-beach-fl\">Fort Myers Beach</a>",
+    "<a href=\"/locations/estero-fl\">Estero</a>"
+  ],
+  "headerImages": [
+    "/src/assets/images/right-accord-health-florida-HOMEPAGE_FIRSTPICTURE.jpg",
+    "/src/assets/images/right-accord-health-florida-HOMEPAGE_ALZHAIMERS.jpg",
+    "/src/assets/images/right-accord-health-florida-HOMEPAGE_PARKINSONS.jpg",
+    "/src/assets/images/right-accord-health-florida-HOMEPAGE_AFTERSTROKE.jpg",
+    "/src/assets/images/right-accord-health-florida-HOMEPAGE_HOSPICE.jpg"
+  ],
+  "headLines": [
+    "In-Home Senior Care in Mcgregor, FL",
+    "Dependable Hourly & 24-Hour Caregivers",
+    "Specialized Alzheimer\u2019s and Dementia Support",
+    "Trusted Care from a Local, Nurse-Owned Agency",
+    "Home Care That Feels Like Family"
+  ],
+  "keywords": [
+    "Mcgregor home health care",
+    "in-home senior care Mcgregor",
+    "dementia care Mcgregor FL",
+    "private duty caregivers Mcgregor",
+    "elderly care Mcgregor Florida",
+    "companion care Mcgregor",
+    "Florida senior care services",
+    "RIGHT ACCORD Mcgregor"
+  ]
+};
+
+export default { data };
