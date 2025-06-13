f13fcbd3e560e43a5be0370505b36ce0174855df
lucascr <lcrodrigues1995@gmail.com>
2025-06-13 12:57:01 -0300

    Create bradenton-fl.js

---
+++ src/data/locations/bradenton-fl.js
@@ -0,0 +1,46 @@
+const data = {
+  metaData: {
+    title: 'Home Health Care Bradenton, FL | RIGHT ACCORD Private Duty Home Health Care',
+    ignoreTitleTemplate: true,
+  },
+  city: 'Bradenton',
+  stateShort: 'FL',
+  stateLong: 'Florida',
+  slug: 'bradenton-fl',
+  county: 'Manatee County',
+  description:
+    "RIGHT ACCORD Home Health Care extends services to Bradenton, FL, offering in-home senior care with compassion and professionalism. Our nurse-led team tailors plans that respect your loved one's dignity and independence.",
+  nearbyLocations: [
+    `<a href="/locations/sarasota-fl">Sarasota</a>`,
+    `<a href="/locations/lakewood-ranch-fl">Lakewood Ranch</a>`,
+    `<a href="/locations/holmes-beach-fl">Holmes Beach</a>`,
+    `<a href="/locations/anna-maria-island-fl">Anna Maria Island</a>`,
+    `<a href="/locations/longboat-key-fl">Longboat Key</a>`,
+  ],
+  headerImages: [
+    '/src/assets/images/right-accord-health-florida-HOMEPAGE_FIRSTPICTURE.jpg',
+    '/src/assets/images/right-accord-health-florida-HOMEPAGE_ALZHAIMERS.jpg',
+    '/src/assets/images/right-accord-health-florida-HOMEPAGE_PARKINSONS.jpg',
+    '/src/assets/images/right-accord-health-florida-HOMEPAGE_AFTERSTROKE.jpg',
+    '/src/assets/images/right-accord-health-florida-HOMEPAGE_HOSPICE.jpg',
+  ],
+  headLines: [
+    'In-Home Senior Care in Bradenton, FL',
+    'Dependable Hourly & 24-Hour Caregivers',
+    'Specialized Alzheimer’s and Dementia Support',
+    'Trusted Care from a Local, Nurse-Owned Agency',
+    'Home Care That Feels Like Family',
+  ],
+  keywords: [
+    'bradenton home health care',
+    'bradenton in-home senior care',
+    'bradenton dementia care',
+    'bradenton private duty caregivers',
+    'bradenton elderly care',
+    'bradenton companion care',
+    'bradenton Florida senior care services',
+    'bradenton RIGHT ACCORD',
+  ],
+};
+
+export default { data };
