f5911b22c1a64ece9d7afeb82b2445be5d6eeef1
lucascr <lcrodrigues1995@gmail.com>
2025-06-13 12:58:20 -0300

    Create fort-myers-fl.js

---
+++ src/data/locations/fort-myers-fl.js
@@ -0,0 +1,45 @@
+const data = {
+  "metaData": {
+    "title": "Home Health Care Fort Myers, FL | RIGHT ACCORD Private Duty Home Health Care",
+    "ignoreTitleTemplate": true
+  },
+  "city": "Fort Myers",
+  "stateShort": "FL",
+  "stateLong": "Florida",
+  "slug": "fort-myers-fl",
+  "county": "Lee County",
+  "description": "RIGHT ACCORD Home Health Care proudly serves Fort Myers with compassionate, in-home care services. Whether your loved one needs hourly support or 24-hour specialized care, we tailor every plan to their unique needs.",
+  "nearbyLocations": [
+    "<a href=\"/locations/mcgregor-fl\">Mcgregor</a>",
+    "<a href=\"/locations/cape-coral-fl\">Cape Coral</a>",
+    "<a href=\"/locations/iona-fl\">Iona</a>",
+    "<a href=\"/locations/babcock-ranch-fl\">Babcock Ranch</a>",
+    "<a href=\"/locations/fort-myers-beach-fl\">Fort Myers Beach</a>"
+  ],
+  "headerImages": [
+    "/src/assets/images/right-accord-health-florida-HOMEPAGE_FIRSTPICTURE.jpg",
+    "/src/assets/images/right-accord-health-florida-HOMEPAGE_ALZHAIMERS.jpg",
+    "/src/assets/images/right-accord-health-florida-HOMEPAGE_PARKINSONS.jpg",
+    "/src/assets/images/right-accord-health-florida-HOMEPAGE_AFTERSTROKE.jpg",
+    "/src/assets/images/right-accord-health-florida-HOMEPAGE_HOSPICE.jpg"
+  ],
+  "headLines": [
+    "In-Home Senior Care in Fort Myers, FL",
+    "Dependable Hourly & 24-Hour Caregivers",
+    "Specialized Alzheimer\u2019s and Dementia Support",
+    "Trusted Care from a Local, Nurse-Owned Agency",
+    "Home Care That Feels Like Family"
+  ],
+  "keywords": [
+    "Fort Myers home health care",
+    "in-home senior care Fort Myers",
+    "dementia care Fort Myers FL",
+    "private duty caregivers Fort Myers",
+    "elderly care Fort Myers Florida",
+    "companion care Fort Myers",
+    "Florida senior care services",
+    "RIGHT ACCORD Fort Myers"
+  ]
+};
+
+export default { data };
